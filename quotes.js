b3b6ca07e205f1c337a9f065d8072ea4d03de1b8
Milos Djuric <milosdjuric3377@gmail.com>
2024-03-02 23:00:45 +0100

    svasta

---
+++ quotes.js
@@ -0,0 +1,114 @@
+const localQuotes = [
+{
+text: ' May the God of hope fill you with all joy and peace as you trust in him, so that you may overflow with hope by the power of the Holy Spirit.',
+author: 'Paul', 
+},
+{
+text: ' For I am convinced that neither death nor life, neither angels nor demons, neither the present nor the future, nor any powers, neither height nor depth, nor anything else in all creation, will be able to separate us from the love of God that is in Christ Jesus our Lord.',
+author: 'Paul', 
+},
+{
+text: 'The steadfast love of the Lord never ceases; his mercies never come to an end; they are new every morning; great is your faithfulness.',
+author: 'Jeremiah', 
+},
+{
+text: ' So we do not lose heart. Though our outer self is wasting away, our inner self is being renewed day by day. For this light momentary affliction is preparing for us an eternal weight of glory beyond all comparison, as we look not to the things that are seen but to the things that are unseen.',
+author: 'Paul', 
+},
+{
+text: 'What, then, shall we say in response to these things? If God is for us, who can be against us?',
+author: 'Paul', 
+},
+{
+text: 'Be strong and courageous. Do not be afraid or terrified because of them, or the Lord your God goes with you; he will never leave you nor forsake you.',
+author: 'Moses', 
+},
+{
+text: 'The Lord is my light and my salvation; whom shall I fear? The Lord is the stronghold of my life; of whom shall I be afraid?',
+author: 'King David', 
+},
+{
+text: 'There is no fear in love. But perfect love drives out fear, because fear has to do with punishment. The one who fears is not made perfect in love.',
+author: 'John', 
+},
+{
+text: ' Fear not, for I am with you; be not dismayed, for I am your God; I will strengthen you, I will help you, I will uphold you with my righteous right hand.',
+author: 'Isaiah', 
+},
+{
+text: 'But they who wait for the Lord shall renew their strength; they shall mount up with wings like eagles; they shall run and not be weary; they shall walk and not faint.',
+author: 'Isaiah', 
+},
+{
+text: 'Greater love has no one than this: to lay down ones life for ones friends.',
+author: 'John', 
+},
+{
+text: 'Jesus looked at them and said, -With man it is impossible, but not with God. For all things are possible with God.-',
+author: 'Mark', 
+},
+{
+text: ' I can do all things through him who strengthens me.',
+author: 'Paul', 
+},
+{
+text: ' Trust in the Lord with all your heart, and do not lean on your own understanding. In all your ways acknowledge him, and he will make straight your paths.',
+author: 'King David', 
+},
+{
+text: 'Be watchful, stand firm in the faith, act like men, be strong. Let all that you do be done in love.',
+author: 'Paul', 
+},
+{
+text: ' Our faith can move mountains.',
+author: 'Matthew', 
+},
+{
+text: ' Taste and see that the Lord is good; blessed is the one who takes refuge in him.',
+author: 'King David', 
+},
+{
+text: ' And we know that in all things God works for the good of those who love him, who have been called according to his purpose.',
+author: 'Paul', 
+},
+{
+text: 'Cast all your anxieties on him, because he cares for you.',
+author: 'Peter', 
+},
+{
+text: ' I have told you all this so that you may have peace in me. Here on earth you will have many trials and sorrows. But take heart, because I have overcome the world.',
+author: 'Jesus', 
+},
+{
+text: 'Come to Me, all you who are weary and burdened, and I will give you rest.',
+author: 'Jesus', 
+},
+{
+text: 'A friend loves at all times, and a brother is born for a time of adversity.',
+author: 'Solomon', 
+},
+{
+text: 'but those who hope in the LORD will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.',
+author: 'Isaiah', 
+},
+{
+text: 'Whoever pursues righteousness and love finds life, prosperity and honor.',
+author: 'Solomon', 
+},
+{
+text: 'When you pass through the waters, I will be with you; and when you pass through the rivers, they will not sweep over you. When you walk through the fire, you will not be burned; the flames will not set you ablaze.',
+author: 'God', 
+},
+{
+text: 'Come to me, all you who are weary and burdened, and I will give you rest.',
+author: 'Matthew', 
+},
+{
+text: 'Jesus replied, “Because you have so little faith. Truly I tell you, if you have faith as small as a mustard seed, you can say to this mountain, ‘Move from here to there,’ and it will move. Nothing will be impossible for you.”',
+author: 'Matthew', 
+},
+
+{
+text: 'The name of the LORD is a fortified tower; the righteous run to it and are safe.', 
+author: 'Solomon',
+} ]
--- script.js
+++ script.js
@@ -1,2 +1,42 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
+function newQuote() {
+  loading();
+  const quote = localQuotes [Math.floor(Math.random() * localQuotes.length)];
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textConent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event listeneres
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+newQuote();
